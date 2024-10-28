cc73deebaa20fe6795d3ca4e37dca3710de41401
Fons Elodie <elodie.fons66@gmail.com>
2024-10-28 17:17:22 +0100

    Commentaires supprimés

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes /* Navigate */ } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import HeaderWH from "./assets/components/Header/HeaderWH"
 import Home from "./assets/pages/Home"
 import EmployeeList from "./assets/pages/EmployeeList"
@@ -7,9 +7,7 @@ function App() {
         <Router>
             <HeaderWH />
             <Routes>
-                <Route path="/" element={<Home />} /> {/* Redirige la racine vers /user/12 a decommenter pour etre sur le Backend*/}
-                {/* <Route path="/error" element={<PageErreur />} />
-                <Route path="*" element={<PageErreur />} /> */}
+                <Route path="/" element={<Home />} />
                 <Route path="/employee-list" element={<EmployeeList />} />
             </Routes>
         </Router>
